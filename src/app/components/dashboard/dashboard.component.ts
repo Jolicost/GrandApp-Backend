a9216acae5dbd74fd47df8e47937fcce944f9eee
Carlos Aires <carlos.ae19@gmail.com>
2018-12-11 19:50:41 +0100

    Dynamic ChartJS implementation

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -20,6 +20,35 @@ export class DashboardComponent implements OnInit {
     entityTypeActValues = [];
     entityHoursActKeys = [];
     entityHoursActValues = [];
+    allBorderColors = [
+        'rgba(183, 28, 28, 1)',
+        'rgba(136, 14, 79, 1)',
+        'rgba(74, 20, 140, 1)',
+        'rgba(26, 35, 126, 1)',
+        'rgba(13, 71, 161, 1)',
+        'rgba(0, 96, 100, 1)',
+        'rgba(0, 77, 64, 1)',
+        'rgba(27, 94, 32, 1)',
+        'rgba(130, 119, 23, 1)',
+        'rgba(245, 127, 23, 1)',
+        'rgba(230, 81, 0, 1)',
+        'rgba(62, 39, 35, 1)'
+    ];
+    allBackgroundColors = [
+        'rgba(183, 28, 28, 0.2)',
+        'rgba(136, 14, 79, 0.2)',
+        'rgba(74, 20, 140, 0.2)',
+        'rgba(26, 35, 126, 0.2)',
+        'rgba(13, 71, 161, 0.2)',
+        'rgba(0, 96, 100, 0.2)',
+        'rgba(0, 77, 64, 0.2)',
+        'rgba(27, 94, 32, 0.2)',
+        'rgba(130, 119, 23, 0.2)',
+        'rgba(245, 127, 23, 0.2)',
+        'rgba(230, 81, 0, 0.2)',
+        'rgba(62, 39, 35, 0.2)'
+    ];
+    size1;
     constructor(
         private userService: UserService,
         private actService: ActivitiesService,
@@ -47,6 +76,15 @@ export class DashboardComponent implements OnInit {
                         this.entityStatsHours
                     );
                     this.totalAct = entityInfo.nActivities;
+                    this.size1 = this.entityTypeActKeys.length;
+                    const borderColorsPick = [];
+                    const backgroundColorsPick = [];
+                    for (let i = 0; i < this.size1; i++) {
+                        borderColorsPick[i] = this.allBorderColors[i];
+                        backgroundColorsPick[i] = this.allBackgroundColors[i];
+                        console.log('Border Pick tenim', borderColorsPick[i]);
+                        console.log('Back Pick tenim', borderColorsPick[i]);
+                    }
                     this.chart = new Chart('myChart', {
                         type: 'bar',
                         data: {
@@ -55,10 +93,8 @@ export class DashboardComponent implements OnInit {
                                 {
                                     label: '# of Activities per Theme',
                                     data: this.entityTypeActValues,
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)'
-                                    ],
-                                    borderColor: ['rgba(255,99,132,1)'],
+                                    backgroundColor: backgroundColorsPick,
+                                    borderColor: borderColorsPick,
                                     borderWidth: 1
                                 }
                             ]
@@ -81,7 +117,7 @@ export class DashboardComponent implements OnInit {
                             labels: this.entityHoursActKeys,
                             datasets: [
                                 {
-                                    label: '# Activities per day',
+                                    label: '# Activities per hour',
                                     data: this.entityHoursActValues,
                                     backgroundColor: [
                                         'rgba(255, 99, 132, 0.2)'

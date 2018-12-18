5f510e828cf2d642051a10a11b5aaa03261f4ecd
Carlos Aires <carlos.ae19@gmail.com>
2018-12-18 20:48:50 +0100

    UserList with User Details

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
     selector: 'app-user-details',
@@ -9,12 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailsComponent implements OnInit {
     sub;
     id;
+    user;
 
-    constructor(private activatedRoute: ActivatedRoute) {}
+    constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {}
 
     ngOnInit() {
         this.sub = this.activatedRoute.params.subscribe(params => {
-            this.id = params['id'];     // aqui tens el id del Usuari
+            this.id = params['id']; // aqui esta el id
+            console.log('El meu id es', this.id);
+            this.userService.getUserInfo(this.id).subscribe(resUI => {
+                this.user = resUI;
+            });
         });
     }
 }

d4f0b88aff654d74e203634d0088ccfa7ba6e247
Carlos Aires <carlos.ae19@gmail.com>
2019-01-07 18:49:48 +0100

    Components refresh

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -5,6 +5,7 @@ import { UserLocationService } from '../../services/userLocation/user-location.s
 import { EntityService } from 'src/app/services/entity/entity.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { DialogService } from 'src/app/services/dialog/dialog.service';
+import { interval } from 'rxjs';
 
 @Component({
     selector: 'app-users',
@@ -12,9 +13,9 @@ import { DialogService } from 'src/app/services/dialog/dialog.service';
     styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-     // google maps zoom level
-     zoom = 13;
-     // initial center position for the map
+    // google maps zoom level
+    zoom = 13;
+    // initial center position for the map
     initLat: number;
     initLong: number;
     usersOfActivity = [];
@@ -26,39 +27,67 @@ export class UsersComponent implements OnInit {
     usersOor;
     mess;
     constructor(
-        private reverseGeoService: ReverseGeocodingService,
-        private activityService: ActivitiesService,
-        private userLocationService: UserLocationService,
         private entityService: EntityService,
         private userService: UserService,
         private dialogService: DialogService
     ) {}
 
     ngOnInit() {
-        this.userService.verify().subscribe (resv => {
+        this.userService.verify().subscribe(resv => {
             this.entityid = resv.entity;
             // console.log('Id entity', this.entityid);
-            this.entityService.getEntityInfo({id: this.entityid}).subscribe (rese => {
-                this.initLat = rese.place.lat;
-                // console.log('Tinc lat', this.initLat);
-                this.initLong = rese.place.long;
-                // console.log('Tinc long', this.initLong);
-            });
-            // console.log('Tinc id entitat', this.entityid);
-            this.entityService.getEmergencyContacts(this.entityid).subscribe (emer => {
-                this.emergencyUsers = emer;
-                console.log('tinc un array de length ', this.emergencyUsers.length);
-                if (this.emergencyUsers.length > 0) {
-                    this.zoom = 9;
-                }
-                this.usersOor = this.emergencyUsers.length;
-                this.mess = 'There are ' + this.usersOor + ' users out of range';
-                this.dialogService.openDialog({
-                    mode: 'infoDialog',
-                    obj: this.mess
+            this.entityService
+                .getEntityInfo({ id: this.entityid })
+                .subscribe(rese => {
+                    this.initLat = rese.place.lat;
+                    // console.log('Tinc lat', this.initLat);
+                    this.initLong = rese.place.long;
+                    // console.log('Tinc long', this.initLong);
                 });
-                console.log('Tinc aquests usuaris perduts', this.usersOor);
-                console.log(this.mess);
+            // console.log('Tinc id entitat', this.entityid);
+            this.entityService
+                .getEmergencyContacts(this.entityid)
+                .subscribe(emer => {
+                    this.emergencyUsers = emer;
+                    console.log(
+                        'tinc un array de length ',
+                        this.emergencyUsers.length
+                    );
+                    this.usersOor = this.emergencyUsers.length;
+                    this.mess =
+                        'There are ' + this.usersOor + ' users out of range';
+                    this.dialogService.openDialog({
+                        mode: 'infoDialog',
+                        obj: this.mess
+                    });
+                    console.log('Tinc aquests usuaris perduts', this.usersOor);
+                    console.log(this.mess);
+                });
+            const source = interval(1000 * 30);
+            const subscribe = source.subscribe(val => {
+                this.entityService
+                    .getEmergencyContacts(this.entityid)
+                    .subscribe(emer => {
+                        this.emergencyUsers = emer;
+                        console.log(
+                            'tinc un array de length ',
+                            this.emergencyUsers.length
+                        );
+                        this.usersOor = this.emergencyUsers.length;
+                        this.mess =
+                            'There are ' +
+                            this.usersOor +
+                            ' users out of range';
+                        this.dialogService.openDialog({
+                            mode: 'infoDialog',
+                            obj: this.mess
+                        });
+                        console.log(
+                            'Tinc aquests usuaris perduts',
+                            this.usersOor
+                        );
+                        console.log(this.mess);
+                    });
             });
         });
     }
